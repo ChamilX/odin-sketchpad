4cdf6c2885d4cce98c799a306baa2b062af33dfb
ChamilX <chamilxavier@gmail.com>
2024-05-24 00:43:30 -0400

    Add prompt to receive size of grid
    Added a prompt to receive the number of blocks per row and per column.

--- code.js
+++ code.js
@@ -1,13 +1,39 @@
 const container = document.querySelector(".container");
 const button = document.querySelector(".create-grid");
+let row = 0;
+let column = 0;
 
 button.addEventListener("click", function() {
     container.innerHTML = "";
-    createGrid();
+    row = prompt("Number of squares per row (max 100)");
+    if (row > 100) {
+        alert("row over a 100!!");
+        row = 4;
+    }
+    column = prompt("Number of squares per column (max 100)");
+    if (column > 100) {
+        alert("column over a 100!!")
+        column = 4;
+    }
+    createGrid(row, column);
 })
 
-function createGrid() {
-    for (i = 0; i < 5 * 5; i++) {
+function calcRow(row) {
+    let width = (50 * row) + 50;
+    width = width.toString();
+    return width + "px";
+}
+
+function calcColumn(column) {
+    let width = (50 * column) - 50;
+    width = width.toString();
+    return width + "px";
+}
+
+function createGrid(rowSize, columnSize) {
+    container.style.width = calcRow(rowSize);
+    container.style.height = calcColumn(columnSize);
+    for (i = 0; i < rowSize * columnSize; i++) {
         const div = document.createElement("div");
         div.classList.add("block");
         div.addEventListener("mouseover", function() {
