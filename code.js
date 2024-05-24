d6a8e3c963d9d54910125b0319cab30faa65eab3
ChamilX <chamilxavier@gmail.com>
2024-05-24 00:12:11 -0400

    Add mouseover event to blocks
    When the event is triggered, the blocks change color.

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ const container = document.querySelector(".container");
 const button = document.querySelector(".create-grid");
 
 button.addEventListener("click", function() {
+    container.innerHTML = "";
     createGrid();
 })
 
@@ -9,6 +10,9 @@ function createGrid() {
     for (i = 0; i < 5 * 5; i++) {
         const div = document.createElement("div");
         div.classList.add("block");
+        div.addEventListener("mouseover", function() {
+            div.setAttribute("class", "block-hover")
+        })
         container.appendChild(div);
     }
 }
